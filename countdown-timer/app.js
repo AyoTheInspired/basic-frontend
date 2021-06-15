9424b0863231cb61e767761e040f33d32728532c
Ayo Abimbola <ayoinspired@gmail.com>
2021-06-15 16:47:30 +0100

    Minor changes

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -1,25 +1,25 @@
 const months = [
- "January",
- "February",
- "March",
- "April",
- "May",
- "June",
- "July",
- "August",
- "September",
- "October",
- "November",
- "December",
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
 ];
 const weekdays = [
- "Sunday",
- "Monday",
- "Tuesday",
- "Wednesday",
- "Thursday",
- "Friday",
- "Saturday",
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
 ];
 
 const giveaway = document.querySelector(".giveaway");
@@ -39,7 +39,6 @@ const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
 
 let month = futureDate.getMonth();
-// console.log(futureDate.getMonth());
 
 month = months[month];
 const date = futureDate.getDate();
@@ -53,40 +52,40 @@ ${date}th ${month} ${year}, ${hours}:${minutes}am`;
 const futureTime = futureDate.getTime();
 
 function getRemainingTime() {
- const today = new Date().getTime();
- const t = futureTime - today;
+	const today = new Date().getTime();
+	const t = futureTime - today;
 
- // values in ms
- const oneDay = 24 * 60 * 60 * 1000;
- const oneHour = 60 * 60 * 1000;
- const oneMinute = 60 * 1000;
+	// values in ms
+	const oneDay = 24 * 60 * 60 * 1000;
+	const oneHour = 60 * 60 * 1000;
+	const oneMinute = 60 * 1000;
 
- // calculate all values
- let days = t / oneDay;
- days = Math.floor(days);
+	// calculate all values
+	let days = t / oneDay;
+	days = Math.floor(days);
 
- let hours = Math.floor((t % oneDay) / oneHour);
+	let hours = Math.floor((t % oneDay) / oneHour);
 
- let minutes = Math.floor((t % oneHour) / oneMinute);
- let seconds = Math.floor((t % oneMinute) / 1000);
+	let minutes = Math.floor((t % oneHour) / oneMinute);
+	let seconds = Math.floor((t % oneMinute) / 1000);
 
- // set values array
- const values = [days, hours, minutes, seconds];
+	// set values array
+	const values = [days, hours, minutes, seconds];
 
- function format(item) {
-  if (item < 10) {
-   return (item = `0${item}`);
-  }
-  return item;
- }
+	function format(item) {
+		if (item < 10) {
+			return (item = `0${item}`);
+		}
+		return item;
+	}
 
- items.forEach(function (item, index) {
-  item.innerHTML = format(values[index]);
- });
- if (t < 0) {
-  clearInterval(countdown);
-  deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
- }
+	items.forEach(function (item, index) {
+		item.innerHTML = format(values[index]);
+	});
+	if (t < 0) {
+		clearInterval(countdown);
+		deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+	}
 }
 
 // countdown
